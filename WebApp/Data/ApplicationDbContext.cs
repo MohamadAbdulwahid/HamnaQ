770478068af6404d67f1738203ae9b4e3b4af0d0
Mohamad Abdulwahid <mohamd.abdulwahid@outlook.com>
2025-07-22 23:05:53 +0200

    Add ApplicationDbContext for Entity Framework Core with Quiz and Question models

---
+++ WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using HamnaQ.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HamnaQ.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Quiz> Quizzes { get; set; }
+    public DbSet<Question> Questions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        // Configure Quiz-Question relationship
+        modelBuilder.Entity<Question>()
+            .HasOne(q => q.Quiz)
+            .WithMany(qz => qz.Questions)
+            .HasForeignKey(q => q.QuizId);
+            
+        // Configure User-Quiz relationship
+        modelBuilder.Entity<Quiz>()
+            .HasOne(q => q.CreatedBy)
+            .WithMany(u => u.CreatedQuizzes)
+            .HasForeignKey(q => q.CreatedById);
+    }
+}
